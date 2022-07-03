e1724196baa84f8f3cfb1d23864a91163786610a
MaxiT <maxitroyt@gmail.com>
2022-07-03 19:28:00 -0300

    Nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { useState } from "react";
+import "./nav.css";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineBook,
+  AiOutlineMessage,
+} from "react-icons/ai";
+import { RiServiceLine } from "react-icons/ri";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [isActive, setIsActive] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setIsActive("#")}
+        className={isActive === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setIsActive("#about")}
+        className={isActive === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setIsActive("#experience")}
+        className={isActive === "#experience" ? "active" : ""}
+      >
+        <AiOutlineBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setIsActive("#services")}
+        className={isActive === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setIsActive("#contact")}
+        className={isActive === "#contact" ? "active" : ""}
+      >
+        <AiOutlineMessage />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
