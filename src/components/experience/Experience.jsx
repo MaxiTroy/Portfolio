2ec570a81a6af54fa6bee68828ef2507bc52846c
MaxiT <maxitroyt@gmail.com>
2022-07-07 10:50:49 -0300

    Experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import "./experience.css";
+import { BsFillPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section id="experience">
+      <h4>What's Skill I Have</h4>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Front-End Development</h3>
+          <div className="experience__content">
+            <article className="experience__detail">
+              <BsFillPatchCheckFill className="experience__icon" />
+              <div>
+                <h4>HTML</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__detail">
+              <BsFillPatchCheckFill className="experience__icon" />
+              <div>
+                <h4>CSS</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__detail">
+              <BsFillPatchCheckFill className="experience__icon" />
+              <div>
+                <h4>Javascript</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__detail">
+              <BsFillPatchCheckFill className="experience__icon" />
+              <div>
+                <h4>React</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__detail">
+              <BsFillPatchCheckFill className="experience__icon" />
+              <div>
+                <h4>Tailwind</h4>
+                <small>Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__detail">
+              <BsFillPatchCheckFill className="experience__icon" />
+              <div>
+                <h4>NextJs</h4>
+                <small>Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__detail">
+              <BsFillPatchCheckFill className="experience__icon" />
+              <div>
+                <h4>Git</h4>
+                <small>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
