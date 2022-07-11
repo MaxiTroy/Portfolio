4f7942f4b49ecd5c67cf7b41496da7ff1cd97c25
MaxiT <maxitroyt@gmail.com>
2022-07-11 11:44:13 -0300

    Services and Portfolio components

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,134 @@
-import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/IMG1.jpg";
+import IMG2 from "../../assets/IMG2.jpg";
+import IMG3 from "../../assets/IMG3.jpg";
+import IMG4 from "../../assets/IMG4.jpg";
+import IMG5 from "../../assets/IMG5.jpg";
+import IMG6 from "../../assets/IMG6.jpg";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section id="portfolio">
+      <h4>My Recent Work</h4>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__img">
+            <img src={IMG1} alt="proyect1" />
+          </div>
+          <h3>Proyect Name</h3>
+          <div className="portfolio__cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__img">
+            <img src={IMG2} alt="proyect1" />
+          </div>
+          <h3>Proyect Name</h3>
+          <div className="portfolio__cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__img">
+            <img src={IMG3} alt="proyect1" />
+          </div>
+          <h3>Proyect Name</h3>
+          <div className="portfolio__cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__img">
+            <img src={IMG4} alt="proyect1" />
+          </div>
+          <h3>Proyect Name</h3>
+          <div className="portfolio__cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__img">
+            <img src={IMG5} alt="proyect1" />
+          </div>
+          <h3>Proyect Name</h3>
+          <div className="portfolio__cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__img">
+            <img src={IMG6} alt="proyect1" />
+          </div>
+          <h3>Proyect Name</h3>
+          <div className="portfolio__cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section id="services">
+      <h4>What I Offer</h4>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="services">
+          <div className="services__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="services__list">
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="services">
+          <div className="services__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="services__list">
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className="services__icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
